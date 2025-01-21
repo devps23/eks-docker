32661524c867bd5a164e96904a02c44c27b6ebca
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-21 09:37:54 +0530

    kubernetes

--- images/http-content/run.sh
+++ images/http-content/run.sh
@@ -1,5 +1,6 @@
 mkdir -p /usr/share/nginx/html
-#while true; do
-#  echo "<h1>Hello pods</h1>">/usr/share/nginx/html/index.html
+#while true ; do
+#  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
+#  sleep 1
 #done
-echo Hello World>/usr/share/nginx/html/index.html
+echo Hello World >/usr/share/nginx/html/index.html
