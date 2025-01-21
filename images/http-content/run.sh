09ba66369892bc12af53b4662df80c4268927c4b
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-21 14:49:38 +0530

    kubernetes

--- images/http-content/run.sh
+++ images/http-content/run.sh
@@ -1,1 +1,6 @@
+#while true ; do
+#  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
+#  sleep 1
+#done
 echo "Hello World">/usr/share/nginx/html/index.html
+#shouldn't give space before path like "echo "Hello World"> /usr/share/nginx/html/index.html" after > shouldn't give space
