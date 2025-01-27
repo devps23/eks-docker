cfef894b092426b4e46d824fc21ae1d653c43db3
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-27 14:56:38 +0530

    eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,77 +1,54 @@
-// create an eks cluster
-resource "aws_eks_cluster" "eks-cluster" {
-  name     = "${var.env}-${var.component}"
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.env}-eks"
   role_arn = aws_iam_role.cluster-role.arn
+
   vpc_config {
     subnet_ids = var.subnet_id
   }
-  #   this is for kms to encrypt disk in an ec2 instance
+
   encryption_config {
     provider {
       key_arn = var.kms_key_id
     }
     resources = ["secrets"]
   }
+
 }
-# //create a security group
-resource "aws_security_group" "security" {
-  name        = "security-${var.component}-${var.env}"
-  description = "security-${var.component}-${var.env}"
-  vpc_id      = var.vpc_id
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+
+resource "aws_launch_template" "main" {
+  name = "eks-${var.env}"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = 100
+      encrypted             = true
+      kms_key_id            = var.kms_key_id
+      delete_on_termination = true
+    }
   }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "sg-${var.component}-eks"
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name" = "${aws_eks_cluster.cluster.name}-workernode"
+    }
   }
 }
 
-# resource "aws_eks_node_group" "eks-node-group" {
-#   cluster_name    = aws_eks_cluster.eks-cluster.name
-#   node_group_name = "${var.env}-${var.component}-nodegrp"
-#   node_role_arn   = aws_iam_role.eks-cluster-role.arn
-#   subnet_ids      = var.subnet_id
-#   instance_types  = ["t3.medium"]
-#   capacity_type   =  "SPOT"
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 2
-#     min_size     = 1
-#   }
-#   launch_template {
-#     version = "$Latest"
-#     name = aws_launch_template.launch_template.name
-#   }
-#   update_config {
-#     max_unavailable = 1
-#   }
-#   tags = {
-#     Name = "${var.env}-${var.component}-nodegrp"
-#   }
-# }
-# create a node group and node group policy
-# attach launch template in node group
 resource "aws_eks_node_group" "main" {
-  cluster_name    = aws_eks_cluster.eks-cluster.name
-  node_group_name = "${var.env}-eks-ng"
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.env}-eks-ng-1"
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_id
   capacity_type   = "SPOT"
-  instance_types  = ["t3.medium"]
+  instance_types  = ["t3.large"]
 
-    launch_template {
-      name    = "${var.env}-${var.component}-template"
-      version = "$Latest"
-    }
+  launch_template {
+    name    = "eks-${var.env}"
+    version = "$Latest"
+  }
 
   scaling_config {
     desired_size = 1
@@ -82,32 +59,5 @@ resource "aws_eks_node_group" "main" {
   update_config {
     max_unavailable = 1
   }
-  tags = {
-    Name = "${var.env}-eks-ng"
-  }
 }
 
-# create a launch template for ami
-resource "aws_launch_template" "launch_template" {
-  name = "${var.env}-${var.component}-template"
-#   disk
-  block_device_mappings {
-    device_name = "/dev/sdf"
-#     ebs storage
-    ebs {
-      volume_size           = 100
-      kms_key_id            = var.kms_key_id
-      encrypted             = true
-      delete_on_termination = true
-    }
-  }
-#   tag specification for instance name
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "${var.env}-${var.component}-template"
-    }
-  }
-}
-
-# kms and ec2 both are services whereas autoscaling group is not a service, so to communicate kms to asg need to add "key users" under kms
