9767fd4e31983361733128a3684542718c6e3492
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-27 14:27:42 +0530

    eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,11 +1,11 @@
 // create an eks cluster
 resource "aws_eks_cluster" "eks-cluster" {
-  name        = "${var.env}-${var.component}"
-  role_arn    = aws_iam_role.eks-cluster-role.arn
+  name     = "${var.env}-${var.component}"
+  role_arn = aws_iam_role.eks-cluster-role.arn
   vpc_config {
     subnet_ids = var.subnet_id
   }
-#   this is for kms to encrypt disk in an ec2 instance
+  #   this is for kms to encrypt disk in an ec2 instance
   encryption_config {
     provider {
       key_arn = var.kms_key_id
@@ -37,16 +37,12 @@ resource "aws_security_group" "security" {
 # create a cluster role and policy
 resource "aws_iam_role" "eks-cluster-role" {
   name               = "${var.env}-instance-role"
-  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.id
+  assume_role_policy = aws_iam_role. eks-cluster-role.arn
   tags = {
     Name = "${var.env}-cluster-role"
   }
 }
-# to create eks in aws we required below policy
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-cluster-role.name
-}
+
 # create a node group and node group policy
 # attach launch template in node group
 # resource "aws_eks_node_group" "eks-node-group" {
@@ -75,13 +71,13 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = "${var.env}-eks-ng"
-  node_role_arn   = aws_iam_role.node-group-role.arn
+  node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_id
   capacity_type   = "SPOT"
   instance_types  = ["t3.medium"]
 
     launch_template {
-      name    = aws_launch_template.launch_template.name
+      name    = "eks-${var.env}"
       version = "$Latest"
     }
 
@@ -98,51 +94,38 @@ resource "aws_eks_node_group" "main" {
     Name = "${var.env}-eks-ng"
   }
 }
-resource "aws_iam_role" "iam_node_role" {
-  name = "eks-node-group-example"
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-# to grant policy in an eks cluster
-resource "aws_iam_role" "node-group-role" {
-  name = "${var.env}-${var.component}-node-grp-role"
-  assume_role_policy = aws_iam_role.iam_node_role.arn
-}
-# the below policy for to interact with eks cluster
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks-cluster-role.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks-cluster-role.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-cluster-role.name
-}
-# create a nodes in node group and iam role
-resource "aws_iam_role" "node" {
-  name              = "${var.env}-${var.component}-node"
- assume_role_policy = aws_iam_role.iam_node_role.arn
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodeMinimalPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
-  role       = aws_iam_role.node.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryPullOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
-  role       = aws_iam_role.node.name
-}
+# # to grant policy in an eks cluster
+# resource "aws_iam_role" "node-group-role" {
+#   name = "${var.env}-${var.component}-node-grp-role"
+#   assume_role_policy = aws_iam_role.iam_node_role.arn
+# }
+# # the below policy for to interact with eks cluster
+# resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#   role       = aws_iam_role.eks-cluster-role.name
+# }
+# resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role       = aws_iam_role.eks-cluster-role.name
+# }
+# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role       = aws_iam_role.eks-cluster-role.name
+# }
+# # create a nodes in node group and iam role
+# resource "aws_iam_role" "node" {
+#   name              = "${var.env}-${var.component}-node"
+#  assume_role_policy = aws_iam_role.iam_node_role.arn
+# }
+# resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodeMinimalPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
+#   role       = aws_iam_role.node.name
+# }
+# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryPullOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+#   role       = aws_iam_role.node.name
+# }
 # create a launch template for ami
 resource "aws_launch_template" "launch_template" {
   name = "${var.env}-${var.component}-template"
