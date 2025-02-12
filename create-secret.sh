0b0d9fd8868c1213af7b1c5508a8548310c82845
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-12 11:37:29 +0530

    jenkins file

--- create-secret.sh
+++ create-secret.sh
@@ -1,1 +1,1 @@
- kubectl create secret  generic  vault-token --from-literal=token=hvs.fMGVYmkvteLqprw3itd1iXUe
+ kubectl create secret  generic  vault-token --from-literal=token=hvs.T8WnPWmYmGZkPYlGAHXVmebk
