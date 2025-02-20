25a7a7a73b82fc46a4c80d4dbddbd2feaf7c9150
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-20 12:12:57 +0530

    backend

--- main.tf
+++ main.tf
@@ -27,12 +27,12 @@ module "rds"{
   storage_type            = "gp3"
   kms_key_id              = var.kms_key_id
 }
-# module "eks"{
-#   source                    =  "./modules/eks"
-#   env                       =  var.env
-#   subnet_id                 =  module.vpc.backend_subnets
-#   component                 = "eks-cluster"
-#   kms_key_id                = var.kms_key_id
-#   vpc_id                    = module.vpc.vpc_id
-#   bastion_nodes             = var.bastion_nodes
-# }
+module "eks"{
+  source                    =  "./modules/eks"
+  env                       =  var.env
+  subnet_id                 =  module.vpc.backend_subnets
+  component                 = "eks-cluster"
+  kms_key_id                = var.kms_key_id
+  vpc_id                    = module.vpc.vpc_id
+  bastion_nodes             = var.bastion_nodes
+}
