cc29dbd35edb2995162c66bef8865cd0a8dd2475
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-21 22:29:28 +0530

    create a secret

--- create-secret.sh
+++ create-secret.sh
@@ -1,1 +1,3 @@
  kubectl create secret  generic  vault-token --from-literal=token=hvs.T8WnPWmYmGZkPYlGAHXVmebk
+#  if pods are created under argocd then vault-token also create under argocd only
+ kubectl create secret  generic  vault-token --from-literal=token=hvs.T8WnPWmYmGZkPYlGAHXVmebk -n argocd
