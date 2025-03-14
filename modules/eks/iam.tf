7a88cbd1a17b9fa2f4484f148cb7f639ab62aeb9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-14 17:07:52 +0530

    host

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -73,26 +73,26 @@ resource "aws_iam_policy" "node-external-dns" {
   {
   "Version": "2012-10-17",
   "Statement": [
-  {
-  "Effect": "Allow",
-  "Action": [
-  "route53:ChangeResourceRecordSets"
-  ],
-  "Resource": [
-  "arn:aws:route53:::hostedzone/Z08520602FC482APPVUI7"
-  ]
-  },
-  {
-  "Effect": "Allow",
-  "Action": [
-  "route53:ListHostedZones",
-  "route53:ListResourceRecordSets",
-  "route53:ListTagsForResource"
-  ],
-  "Resource": [
-  "*"
-  ]
-  }
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets"
+      ],
+      "Resource": [
+        "arn:aws:route53:::hostedzone/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets",
+        "route53:ListTagsForResource"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
   ]
 }
 EOF
