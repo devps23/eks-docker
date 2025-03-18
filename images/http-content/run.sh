f6a5fe035409a9220e3a371348f430ffba946252
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-18 09:39:15 +0530

    create a pod

--- images/http-content/run.sh
+++ images/http-content/run.sh
@@ -1,6 +1,6 @@
 #while true ; do
-#  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
+#  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/run.sh
 #  sleep 1
 #done
 echo "Hello World">/usr/share/nginx/html/index.html
-#shouldn't give space before path like "echo "Hello World"> /usr/share/nginx/html/index.html" after > shouldn't give space
+#shouldn't give space before path like "echo "Hello World"> /usr/share/nginx/html/run.sh" after > shouldn't give space
