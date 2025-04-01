0b2a5131ee33567dc697ad48ff74ec9a0c6000d7
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-04-01 15:18:31 +0530

    monitoring tools

--- modules/eks/external-dns.tf
+++ modules/eks/external-dns.tf
@@ -54,7 +54,7 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   set {
     name  = "service.type"
-    value = "NodePort"
+    value = "LoadBalancer"
 
   }
 }
