4ed3af610a1743b48bc41a9bccf86ecab82ee3d7
Your Name <vemasani.pavani@gmail.com>
2025-04-24 11:59:33 +0530

    public subnet required

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,17 +45,17 @@ resource "aws_subnet" "backend_subnets" {
 #     Name = "${var.env}-mysql-subnets-${count.index}"
 #   }
 # }
-# create public subnets
-# resource "aws_subnet" "public_subnets" {
-#   count                = length(var.public_subnets)
-#   vpc_id               = aws_vpc.vpc.id
-#   cidr_block           = var.public_subnets[count.index]
-#   availability_zone    = var.availability_zone[count.index]
-#
-#   tags = {
-#     Name = "${var.env}-public-subnets-${count.index}"
-#   }
-# }
+create public subnets
+resource "aws_subnet" "public_subnets" {
+  count                = length(var.public_subnets)
+  vpc_id               = aws_vpc.vpc.id
+  cidr_block           = var.public_subnets[count.index]
+  availability_zone    = var.availability_zone[count.index]
+
+  tags = {
+    Name = "${var.env}-public-subnets-${count.index}"
+  }
+}
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
