aa1db95c53a05641c4916b2e97f8a97569696550
Your Name <vemasani.pavani@gmail.com>
2025-09-06 16:23:49 +0530

    roboshop

--- create-secret.sh
+++ create-secret.sh
@@ -1,3 +1,4 @@
  kubectl create secret  generic  vault-token --from-literal=token=hvs.T8WnPWmYmGZkPYlGAHXVmebk
 #  if pods are created under argocd then vault-token also create under argocd only
  kubectl create secret  generic  vault-token --from-literal=token=hvs.T8WnPWmYmGZkPYlGAHXVmebk -n argocd
+hvs.T8WnPWmYmGZkPYlGAHXVmebk
